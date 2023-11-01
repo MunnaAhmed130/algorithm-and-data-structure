72d37d0dd5f97ef7c63c8cf81eb8aa740944aa6c
Munna Ahmed <munnaahmed2025@gmail.com>
2023-11-01 21:19:08 +0600

    greedy algo

--- GreedyAlgorithms/largestPermutation.js
+++ GreedyAlgorithms/largestPermutation.js
@@ -7,34 +7,36 @@ const largestPermutation = (arr, steps) => {
     }
 
     let i = 0;
-    console.log(pos, i, arr);
+    console.log(pos, arr);
+    // let count = 0;
+    while (steps && i < arr.length - 1) {
+        let j = pos[max]; // pos of max num { '1': 3, '2': 1, '3': 0, '4': 2, '5': 4 }
+        console.log("i =", i, "j =", j);
 
-    while (steps && i < arr.length) {
-        let j = pos[max];
-        // console.log(pos, "i =", i, "j =", j);
-
-        if (i == j) {
-            continue;
-        } else {
+        if (i != j) {
             steps--;
-
+            count++;
+            // console.log(arr[i], pos[arr[i]]);
             let temp;
-            temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+            temp = arr[i]; // temp = arr[0]/3
+            arr[i] = arr[j]; // arr[0] = arr[4]/5
+            arr[j] = temp; // arr[4]  = 3
 
-            temp = pos[arr[i]];
-            pos[arr[i]] = pos[arr[j]];
-            pos[arr[j]] = temp;
+            temp = pos[arr[i]]; // temp = pos[3]/0
+            pos[arr[i]] = pos[arr[j]]; // pos[3] = 4
+            pos[arr[j]] = temp; // pos[5] = 0
         }
 
         i++;
         max--;
-        console.log(i, max);
+        console.log(pos, arr);
+        console.log(i, steps, max);
     }
     return arr;
+    // return [arr, count];
 };
 
-// const array = [3, 2, 4, 1, 5];
-const array = [3, 2, 4, 5, 1];
-console.log(largestPermutation(array, 1));
+const array = [3, 2, 4, 1, 5];
+// const array = [3, 2, 4, 5, 1];
+// 3
+console.log(largestPermutation(array, 10));
