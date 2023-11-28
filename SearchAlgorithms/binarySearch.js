0cf9d94e1dfc06698755306369710cdeadb11409
Munna Ahmed <munnaahmed2025@gmail.com>
2023-11-29 03:01:44 +0600

    commit

--- SearchAlgorithms/binarySearch.js
+++ SearchAlgorithms/binarySearch.js
@@ -1,3 +1,10 @@
+// let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+
+// in math both are ok but in computer science below equation is used to avoid integer overflow
+// let middleIndex = Math.floor((leftIndex + (rightIndex - leftIndex)) / 2);
+
+let m = Math.floor((0 + (Number.MAX_SAFE_INTEGER - 0)) / 2);
+console.log(m, Number.MAX_SAFE_INTEGER + 2 === Number.MAX_SAFE_INTEGER + 3);
 function binarySearch(arr, target) {
     let leftIndex = 0;
     let rightIndex = arr.length - 1;
--- kata-machine/src/day1/LinearSearchList.ts
+++ kata-machine/src/day1/LinearSearchList.ts
@@ -1,3 +1,14 @@
-export default function linear_search(haystack: number[], needle: number): boolean {
-
+export default function linear_search(
+    haystack: number[],
+    needle: number,
+): boolean {
+    for (let i = 0; i < haystack.length; ++i) {
+        if (haystack[i] === needle) {
+            return true;
+        }
+        // console.log(i,haystack[i] );
+    }
+    return false;
 }
+
+console.log(linear_search([15, 8, 4, 5], 4));
