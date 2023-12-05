d350241f72e8e7b907e97048aba1d795f89f548b
Munna Ahmed <munnaahmed2025@gmail.com>
2023-12-05 19:06:16 +0600

    commit

---
+++ DS/custom/hash-table2.js
@@ -0,0 +1,73 @@
+class HashTable {
+    constructor(size) {
+        this.table = new Array(size);
+        this.size = size;
+    }
+    // hashing collision
+    hash(key) {
+        let total = 0;
+        for (let i = 0; i < key.length; ++i) {
+            total += key.charCodeAt(i);
+        }
+        return total % this.size;
+    }
+
+    set(key, value) {
+        let index = this.hash(key);
+        const bucket = this.table[index];
+        if (!bucket) {
+            this.table[index] = [[key, value]];
+        } else {
+            const sameKeyItem = bucket.find((item) => item[0] === key);
+            if (sameKeyItem) {
+                sameKeyItem[1] = value;
+            } else {
+                bucket.push([key, value]);
+            }
+        }
+        // this.table[index] = value;
+    }
+
+    get(key) {
+        let index = this.hash(key);
+        const bucket = this.table[index];
+        if (bucket) {
+            const sameKeyItem = bucket.find((item) => item[0] === key);
+            if (sameKeyItem) {
+                return sameKeyItem[1];
+            }
+        }
+        return undefined;
+    }
+
+    remove(key) {
+        let index = this.hash(key);
+        const bucket = this.table[index];
+        if (bucket) {
+            const sameKeyItem = bucket.find((item) => item[0] === key);
+            if (sameKeyItem) {
+                bucket.splice(bucket.indexOf(sameKeyItem), 1);
+            }
+        }
+        // this.table[index] = undefined;
+    }
+
+    display() {
+        for (let i = 0; i < this.table.length; ++i) {
+            if (this.table[i]) {
+                console.log(i, this.table[i]);
+            }
+        }
+    }
+}
+
+const table = new HashTable(50);
+table.set("name", "adam");
+table.set("enam", "ada");
+table.set("age", 20);
+console.log(table.get("name"));
+table.display();
+// console.log(table);
+table.remove("name");
+table.remove("enam");
+table.display();
